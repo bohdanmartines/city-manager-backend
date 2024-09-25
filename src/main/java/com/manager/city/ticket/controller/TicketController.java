d931588d1faeb4eede9e2c09f26a025699c2da18
Bohdan Martines <bohdan.martines@gmail.com>
2024-09-25 15:53:59 +0200

    Add an endpoint to create new tickets

--- src/main/java/com/manager/city/ticket/controller/TicketController.java
+++ src/main/java/com/manager/city/ticket/controller/TicketController.java
@@ -1,16 +1,25 @@
 package com.manager.city.ticket.controller;
 
 import com.manager.city.ticket.dto.TicketDto;
+import com.manager.city.ticket.dto.CreateTicketDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/ticket")
 public class TicketController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TicketController.class);
 
     private static final List<TicketDto> MOCK_TICKETS = List.of(
             new TicketDto(1, "Install More Public Benches", "Request to install more benches in the city parks for elderly and families.", "Open", "2024-09-23T10:00:00Z"),
@@ -21,5 +30,11 @@ public class TicketController {
     @GetMapping
     public ResponseEntity<List<TicketDto>> messages() {
         return ResponseEntity.ok().body(MOCK_TICKETS);
+    }
+
+    @PostMapping("new")
+    public ResponseEntity<Long> createTicket(@RequestBody CreateTicketDto ticketDto) throws URISyntaxException {
+        LOGGER.info("Received a call to create a ticket [{}]", ticketDto);
+        return ResponseEntity.created(URI.create("/api/ticket/" + 1)).body(1L);
     }
 }
---
+++ src/main/java/com/manager/city/ticket/dto/CreateTicketDto.java
@@ -0,0 +1,4 @@
+package com.manager.city.ticket.dto;
+
+public record CreateTicketDto(String title, String description) {
+}
