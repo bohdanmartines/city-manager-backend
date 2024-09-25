2eb749772f650e6c65c8a5f3d4cc26edc10c3a84
Bohdan Martines <bohdan.martines@gmail.com>
2024-09-25 15:16:51 +0200

    Move TicketDto to a new dto package

--- src/main/java/com/manager/city/ticket/controller/TicketController.java
+++ src/main/java/com/manager/city/ticket/controller/TicketController.java
@@ -1,5 +1,6 @@
 package com.manager.city.ticket.controller;
 
+import com.manager.city.ticket.dto.TicketDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/manager/city/ticket/dto/TicketDto.java
+++ src/main/java/com/manager/city/ticket/dto/TicketDto.java
@@ -1,4 +1,4 @@
-package com.manager.city.ticket.controller;
+package com.manager.city.ticket.dto;
 
 public record TicketDto(long id, String title, String description, String status, String createdAt) {
 }
